b7b8e788df8b3faf8cd53796ea32bff79141e646
Mochamad Gufron <mgufronefendi@gmail.com>
2020-04-16 16:38:52 +0700

    fix: use passed directory on iOS

--- ios/Classes/SwiftTesseractOcrPlugin.swift
+++ ios/Classes/SwiftTesseractOcrPlugin.swift
@@ -8,45 +8,31 @@ public class SwiftTesseractOcrPlugin: NSObject, FlutterPlugin {
         let instance = SwiftTesseractOcrPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        initializeTessData()
         if call.method == "extractText" {
-            
+
             guard let args = call.arguments else {
                 result("iOS could not recognize flutter arguments in method: (sendParams)")
                 return
             }
-            
+
             let params: [String : Any] = args as! [String : Any]
             let language: String? = params["language"] as? String
-            var swiftyTesseract = SwiftyTesseract(language: .english)
+            let path: String? = params["tessData"] as? String
+            let customBundle: Bundle = Bundle.init(path: path!)!
+            var swiftyTesseract = SwiftyTesseract(language: .english, bundle: customBundle)
             if(language != nil){
-                swiftyTesseract = SwiftyTesseract(language: .custom(language as String!))
+                swiftyTesseract = SwiftyTesseract(language: .custom(language as String!), bundle: customBundle)
             }
             let  imagePath = params["imagePath"] as! String
             guard let image = UIImage(contentsOfFile: imagePath)else { return }
-            
+
             swiftyTesseract.performOCR(on: image) { recognizedString in
-                
+
                 guard let extractText = recognizedString else { return }
                 result(extractText)
             }
-        }
-    }
-    
-    func initializeTessData() {
-        
-        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-        let destURL = documentsURL!.appendingPathComponent("tessdata")
-        
-        let sourceURL = Bundle.main.bundleURL.appendingPathComponent("tessdata")
-        
-        let fileManager = FileManager.default
-        do {
-            try fileManager.createSymbolicLink(at: sourceURL, withDestinationURL: destURL)
-        } catch {
-            print(error)
         }
     }
 }
